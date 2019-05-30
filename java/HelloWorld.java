0c2b3778a52b2a6f2f9710f44fd16c4008d1f1a0
Chaitanya Sharma <chaitanya@3bandar.org>
2019-05-30 20:01:10 +0530

    fixed indentation

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -3,18 +3,17 @@ public class HelloWorld {
     private final static String greeting = "Hello World!";
     
     public int add(int a, int b) {
-	return a + b;
+	    return a + b;
     }
 
     public int multiply(int a) {
-	return a * 5;
+    	return a * 5;
     }
 
-
     public static void main(String[] args) {
-	HelloWorld hw = new HelloWorld();
-	int result1 = hw.add(100, 50);
-	int result2 = hw.multiply(result1);
-	System.out.println("Hello World! " + result2);
+        HelloWorld hw = new HelloWorld();
+        int result1 = hw.add(100, 50);
+        int result2 = hw.multiply(result1);
+        System.out.println("Hello World! " + result2);
     }
 }
