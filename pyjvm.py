fe791a27962ddfe8b070e68453aa1ce668c88004
Chaitanya Sharma <chaitanya@3bandar.org>
2020-03-03 13:35:45 +0530

    refactored pyjvm.py using click.commands

--- pyjvm.py
+++ pyjvm.py
@@ -1,6 +1,6 @@
-#!//usr/bin/env python
+#!/usr/bin/env python
 
-import os, sys
+import os, sys, click
 from pyjvm.rt.intrptr import Intrptr
 from pyjvm.rt.klassrepo import SharedRepo
 from pyjvm.utils.help import usage
@@ -8,24 +8,32 @@ from pyjvm.utils.reader import FileReader
 from pyjvm.loader.parser import PyParser
 
 
-if __name__ == '__main__':
-    args = sys.argv
-    if len(args) >= 2 and not args[1].endswith(".class"):
-        usage()
-        
-    bytes = FileReader.read(args[1])
-    parser = PyParser(bytes).parse()
-    pyklass = parser.build()
+def __init_repo():
+    return SharedRepo()
 
-    repo = SharedRepo()
+def __init_intrptr(repo):
+    return Intrptr(repo)
+
+def __parse_klass(klass):
+    bytes = FileReader.read(klass)
+    parser = PyParser(bytes).parse()
+    return parser.build()
+
+@click.command()
+@click.argument('klass')
+@click.argument('func')
+def execute(klass, func):
+    repo = __init_repo()
+    intrptr = __init_intrptr(repo)
+    
+    pyklass = __parse_klass(klass)
     repo.add(klass=pyklass)
 
-    intrptr = Intrptr(repo)
-    print(pyklass.get_methods())
-    # method = pyklass.get_method("soo:()Ljava/lang/String;")
-    # method = pyklass.get_method("ifelse4:()I")
-    method = pyklass.get_method("foo:()I")
-
+    method = pyklass.get_method(func)
     result = intrptr.execute(method)
     print("execution complete with result: {}".format(result))
+
+
+if __name__ == '__main__':
+    execute()
 
